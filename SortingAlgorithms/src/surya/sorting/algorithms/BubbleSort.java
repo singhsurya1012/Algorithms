3c1556f77248f34c9dc54a407aec787f4cd62831
Surya Singh <singhsurya1012@gmail.com>
2021-01-17 04:28:04 +0530

    Bubble sort updated

--- SortingAlgorithms/src/surya/sorting/algorithms/BubbleSort.java
+++ SortingAlgorithms/src/surya/sorting/algorithms/BubbleSort.java
@@ -18,10 +18,11 @@ public class BubbleSort implements Sorter{
 	public void sortArray(int[] arr) {
 		
 		boolean numSwitched;
+		int n = arr.length;
 		do {
 			numSwitched = false;
 			//here we don't have to compare for the last element hence length-1 is used
-			for(int i=0;i<arr.length-1;i++) {
+			for(int i=0;i<n-1;i++) {
 				if(arr[i] > arr[i+1]) {
 					int temp =arr[i+1];
 					arr[i+1] = arr[i];
@@ -29,7 +30,7 @@ public class BubbleSort implements Sorter{
 					numSwitched = true;
 				}
 			}
-
+			n--;
 		} while (numSwitched);
 	}
 
